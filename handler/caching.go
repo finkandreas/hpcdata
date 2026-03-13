bc7ddc737ff0d8430e2daa29ea2c8df4f1380fea
Andreas Fink <finkandreas@web.de>
2026-03-13 13:01:27 +0100

    more caching debug messages

--- handler/caching.go
+++ handler/caching.go
@@ -30,29 +30,31 @@ func InitRedis(cfg util.RedisConfig) {
 }
 
 // getter must return a pointer to the type of interest + a caching duration + error if no caching should be done
-func get_cached[T any](job_key string, logger *zerolog.Logger, getter func() (*T, time.Duration, error)) (*T, error) {
+func get_cached[T any](cache_key string, logger *zerolog.Logger, getter func() (*T, time.Duration, error)) (*T, error) {
+	logger.Debug().Msgf("Requesting cache data for cache_key=%v", cache_key)
 	ctx := context.Background()
-	if cached_job_data, err := redis_client.Get(ctx, job_key).Bytes(); err == nil {
-		logger.Debug().Msgf("Found cached data for job_key=%v", job_key)
+	if cached_job_data, err := redis_client.Get(ctx, cache_key).Bytes(); err == nil {
+		logger.Debug().Msgf("Found cached data for cache_key=%v", cache_key)
 		buf := bytes.NewBuffer(cached_job_data)
 		var ret T
 		if err := gob.NewDecoder(buf).Decode(&ret); err != nil {
 			// warn in the log file, but ignore error and fetch freshly from f7t/elastic
-			logger.Warn().Err(err).Msgf("Could not decode cached data for job_key=%v", job_key)
+			logger.Warn().Err(err).Msgf("Could not decode cached data for cache_key=%v", cache_key)
 		} else {
 			return &ret, nil
 		}
 	}
 	// we end up here if cached data is not found, or could not be used - get it from the real implementation
 	if ret, cache_timeout, err := getter(); err != nil {
-		logger.Warn().Err(err).Msgf("Got an error trying to receive data for caching. job_key=%v", job_key)
+		logger.Warn().Err(err).Msgf("Got an error trying to receive data for caching. cache_key=%v", cache_key)
 		return ret, err
 	} else {
 		buf := bytes.NewBuffer(nil)
 		if err := gob.NewEncoder(buf).Encode(*ret); err != nil {
 			logger.Error().Err(err).Msgf("Failed encoding cache value with gob encoding")
 		} else {
-			redis_client.Set(ctx, job_key, buf.Bytes(), cache_timeout)
+			logger.Debug().Msgf("Storing cached data for cache_key=%v", cache_key)
+			redis_client.Set(ctx, cache_key, buf.Bytes(), cache_timeout)
 		}
 		return ret, nil
 	}
