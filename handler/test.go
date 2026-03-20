98e108ab52f4f24a062980de43016836ab9efbde
Andreas Fink <finkandreas@web.de>
2026-03-20 16:29:52 +0100

    add logging to test handler

--- handler/test.go
+++ handler/test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+
+	"cscs.ch/hpcdata/logging"
 )
 
 type test struct{}
@@ -13,15 +15,19 @@ func GetTestHandler() func(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h test) Get(w http.ResponseWriter, r *http.Request) {
+	logger := logging.GetReqLogger(r)
+
 	resp_code := r.URL.Query().Get("response_code")
+	logger.Debug().Msgf("Received resp_code=%v from request", resp_code)
 	resp_code_int := http.StatusBadGateway
 	if resp_code != "" {
 		var err error
 		if resp_code_int, err = strconv.Atoi(resp_code); err != nil {
+			logger.Warn().Err(err).Msgf("Could not convert to an integer")
 			resp_code_int = http.StatusBadGateway
 		}
 	}
 
 	time.Sleep(1 * time.Second)
-	http.Error(w, "Gateway timeout", resp_code_int)
+	http.Error(w, resp_code, resp_code_int)
 }
